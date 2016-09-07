9e7c517e48bd08cbc611addb9a678fe4e811e89d
Jamal Senouci <jamalsenouci@gmail.com>
2016-09-07 07:19:42 +0100

    tidied up init.py

--- epsilon/__init__.py
+++ epsilon/__init__.py
@@ -1,6 +1,5 @@
 from epsilon.model import Model
 from epsilon.data import Data
-from epsilon.parser import * 
-import bokeh
-import bokeh.plotting as bk
-bk.output_notebook()
+from epsilon.parser import *
+from bokeh.plotting import output_notebook
+output_notebook()
