f81e82b6faa7523b03058bd85a55f6533edc7ac3
Jamal Senouci <jamalsenouci@gmail.com>
2016-09-11 20:34:18 +0100

    added some plotting tests

---
+++ epsilon/tests/test_plotting.py
@@ -0,0 +1,32 @@
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+from epsilon.core import EO
+import pandas as pd
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def combined():
+    ints = [[1, 2], [3, 4], [5, 6]]
+    df = pd.DataFrame(ints,
+                      index=["1999-01-01", "1999-01-02", "1999-01-03"],
+                      columns=['Sales', 'Marketing Spend'])
+    df.iloc[2, :] = np.nan
+    eo = EO(df)
+    eo.model.dep('sales')
+    eo.model.ols()
+    obs = eo.model.obs()
+    actual = eo.model.depvar[eo.model.sample[0]]
+    predict = eo.model.fitdetail.predict()
+    model = pd.Series(predict, index=obs, name='Model')
+    combined = pd.concat([actual, model], axis=1)
+    return combined
+
+
+def test_avm_shape(combined):
+    assert (combined.shape == (2, 2))
+
+
+def test_avm_val(combined):
+    print(combined.values)
+    assert_array_almost_equal(combined.values, np.array([[1., 2.], [3., 2.]]))
